43ff61005afd3e1c03b528a0cad9834ef11b8d04
Mathcegep <144719358+Mathcegep@users.noreply.github.com>
2023-10-10 17:16:44 -0400

    creation SQL sans tables

---
+++ SQLDatabase/main.js
@@ -0,0 +1,15 @@
+var mysql = required("mysql");
+console.log("Getting connection......");
+
+var conn = mysql.createConnection({
+    database: "defaultdb",
+    host: "mysql-2c350324-cegeplimoilou-tp2-mb-ac-e.aivencloud.com",
+    user: "mainuser",
+    password: "AVNS_dy1bVOkVtP4zbEFvpzf",
+    port: 18745
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!")
+})
