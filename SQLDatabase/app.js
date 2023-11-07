5101654f1b36d10dd6c7d2445aef2d28e6eab05b
AC-climoilou <1701633@cegeplimoilou.ca>
2023-11-07 06:59:24 -0500

    Update app.js

--- SQLDatabase/app.js
+++ SQLDatabase/app.js
@@ -28,8 +28,6 @@ app.use(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
